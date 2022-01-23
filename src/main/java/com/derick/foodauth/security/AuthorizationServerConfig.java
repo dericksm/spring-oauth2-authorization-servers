9e6888d7175ca8416cf9acf4a0915faf8f1158d4
Derick Souza <derick_sm@hotmail.com>
2022-01-23 20:53:35 -0300

    Authorization Code with PKCE

--- src/main/java/com/derick/foodauth/security/AuthorizationServerConfig.java
+++ src/main/java/com/derick/foodauth/security/AuthorizationServerConfig.java
@@ -2,6 +2,7 @@ package com.derick.foodauth.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +11,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
@@ -24,6 +31,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
@@ -34,17 +44,20 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                         .scopes("write", "read")
                         .accessTokenValiditySeconds(60 * 60 * 6)
                         .refreshTokenValiditySeconds(60 * 24 * 60 * 1)
+
                     .and()
                         .withClient("food")
                         .secret(passwordEncoder.encode("web123"))
                         .authorizedGrantTypes("client_credentials")
                         .scopes("write", "read")
+
                     .and()
                         .withClient("foodanalytics")
                         .secret(passwordEncoder.encode("web123"))
                         .authorizedGrantTypes("authorization_code")
                         .scopes("write", "read")
                         .redirectUris("http://teste")
+
                     .and()
                     .withClient("checktoken")
                         .secret(passwordEncoder.encode("check123"));
@@ -56,11 +69,4 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         security.checkTokenAccess("permitAll()");
     }
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints
-                .authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService)
-                .reuseRefreshTokens(false);
-    }
 }
