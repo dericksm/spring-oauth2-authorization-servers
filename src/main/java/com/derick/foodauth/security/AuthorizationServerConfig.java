99781acf23eb3cc21edc7ad33c8bf2ff977778a5
Derick Souza <derick_sm@hotmail.com>
2022-01-23 21:08:05 -0300

    Fixed configuration

--- src/main/java/com/derick/foodauth/security/AuthorizationServerConfig.java
+++ src/main/java/com/derick/foodauth/security/AuthorizationServerConfig.java
@@ -2,7 +2,6 @@ package com.derick.foodauth.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,12 +10,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.CompositeTokenGranter;
-import org.springframework.security.oauth2.provider.TokenGranter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
@@ -31,42 +24,47 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @Autowired
-    private RedisConnectionFactory redisConnectionFactory;
-
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .inMemory()
-                    .withClient("food-web")
-                        .secret(passwordEncoder.encode("web123"))
-                        .authorizedGrantTypes("password", "refresh_token")
-                        .scopes("write", "read")
-                        .accessTokenValiditySeconds(60 * 60 * 6)
-                        .refreshTokenValiditySeconds(60 * 24 * 60 * 1)
+                .withClient("food-web")
+                .secret(passwordEncoder.encode("web123"))
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("write", "read")
+                .accessTokenValiditySeconds(60 * 60 * 6)
+                .refreshTokenValiditySeconds(60 * 24 * 60 * 1)
 
-                    .and()
-                        .withClient("food")
-                        .secret(passwordEncoder.encode("web123"))
-                        .authorizedGrantTypes("client_credentials")
-                        .scopes("write", "read")
+                .and()
+                .withClient("food")
+                .secret(passwordEncoder.encode("web123"))
+                .authorizedGrantTypes("client_credentials")
+                .scopes("write", "read")
 
-                    .and()
-                        .withClient("foodanalytics")
-                        .secret(passwordEncoder.encode("web123"))
-                        .authorizedGrantTypes("authorization_code")
-                        .scopes("write", "read")
-                        .redirectUris("http://teste")
+                .and()
+                .withClient("foodanalytics")
+                .secret(passwordEncoder.encode("web123"))
+                .authorizedGrantTypes("authorization_code")
+                .scopes("write", "read")
+                .redirectUris("http://teste")
 
-                    .and()
-                    .withClient("checktoken")
-                        .secret(passwordEncoder.encode("check123"));
+                .and()
+                .withClient("checktoken")
+                .secret(passwordEncoder.encode("check123"));
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 //		security.checkTokenAccess("isAuthenticated()");
         security.checkTokenAccess("permitAll()");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints){
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
+                .reuseRefreshTokens(false);
     }
 
 }
